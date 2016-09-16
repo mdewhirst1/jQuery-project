38b06782e821c853a3369ba8acc5c846b6b721e1
Matthew Dewhirst <matthew.dewhirst@skybettingandgaming.com>
2016-09-16 12:10:20 +0100

    made page changing better

--- js/app.js
+++ js/app.js
@@ -1,18 +1,27 @@
 $(function(){
+	var main = $('#main');
+	var other = $('#other');
+	var somethingElse = $('#somethingElse');
+	setPage();
 	addEventListeners();
 });
 
+var setPage = function() {
+	$(other).slideUp();
+	$(somethingElse).slideUp();
+}
+
 var addEventListeners = function() {
 	$('#mainLink').on('click', function() {
-		$('#main').removeClass('hidden');
-		$('#main').siblings().addClass('hidden');
+		$(main).slideDown();
+		$(main).siblings().slideUp();
 	});
 	$('#otherLink').on('click', function() {
-		$('#other').removeClass('hidden');
-		$('#other').siblings().addClass('hidden');
+		$(other).slideDown();
+		$(other).siblings().slideUp();
 	});
 	$('#somethingElseLink').on('click', function() {
-		$('#somethingElse').removeClass('hidden');
-		$('#somethingElse').siblings().addClass('hidden');
+		$(somethingElse).slideDown();
+		$(somethingElse).siblings().slideUp();
 	});
 }
